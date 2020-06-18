c645b470e45c3137db8b502a8b8760b397720d4c
Robin Millette <robin@millette.info>
2020-06-18 08:54:18 -0400

    Update snowpack config.

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,7 +1,18 @@
 module.exports = {
+  exclude: [
+    "cc-v2.code-workspace",
+  ],
+  scripts: {
+    "mount:src": "mount src --to /",
+    "mount:css": "mount css",
+    "mount:credits": "mount target/credits --to /credits",
+    "run:sass": "sass src/sass:css --no-source-map",
+    "run:sass::watch": "$1 --watch",
+  },
   devOptions: {
-    fallback: "src/index.html",
+    // fallback: "index.html",
+    bundle: false,
     open: "none",
-    hmr: false
-  }
+    hmr: false,
+  },
 }
