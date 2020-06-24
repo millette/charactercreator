662546a4e0054cafc0981810793617598766db24
Robin Millette <robin@millette.info>
2020-06-24 17:56:37 -0400

    Export sidebar functions.

--- src/js/sidebars.js
+++ src/js/sidebars.js
@@ -1,15 +1,14 @@
-
-function showSidebarLeft () {
+export function showSidebarLeft () {
   var sidebarLeft = document.querySelector('#sidebar-left')
   sidebarLeft.classList.add('visible')
 }
 
-function hideSidebarLeft () {
+export function hideSidebarLeft () {
   var sidebarLeft = document.querySelector('#sidebar-left')
   sidebarLeft.classList.remove('visible')
 }
 
-function clearSidebarLeft () {
+export function clearSidebarLeft () {
   var sidebarLeft = document.querySelector('#sidebar-left')
   sidebarLeft.innerHTML = ''
 }
@@ -19,12 +18,12 @@ function showSidebarRight () {
   sidebarLeft.classList.add('visible')
 }
 
-function hideSidebarRight () {
+export function hideSidebarRight () {
   var sidebarLeft = document.querySelector('#sidebar')
   sidebarLeft.classList.remove('visible')
 }
 
-function clearSidebarRight () {
+export function clearSidebarRight () {
   var sidebarContent = document.querySelector('#content_1')
   var sidebarRight = document.querySelector('#sidebar')
   sidebarRight.classList.remove('visible')
