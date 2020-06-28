2e779e3642785f5e3a1ff696cc0a41e5d3757ba0
Robin Millette <robin@millette.info>
2020-06-28 10:07:17 -0400

    Satisfy linter (mostly) for logic.js.

--- src/js/logic.js
+++ src/js/logic.js
@@ -1,14 +1,23 @@
+// TODO: import { zoomFull } from zoom.js
+// TODO: import { clearCharacter } from layers.js
+// TODO: import { interpretHash } from parse_hash.js
+// TODO: import { hideSidebarLeft, hideSidebarRight, clearSidebarLeft, clearSidebarRight, showSidebarLeft } from sidebars.js
+// TODO: import { createForm, getSectionLayersList } from form.js
+// TODO: import { capitalizeFirstLetter } from helper.js
+// TODO: import { getMultiLayer } from main.js
+// TODO: import { triggerSaveBtn } from login.js
+// TODO: import { changeClipPathOnEyes } from clippaths.js
 
 // Change sex of character from male to female or vice versa.
-function trans (sex) {
-  if (c.choices.sex === sex) { return }
+export function trans (sex) {
+  if (window.c.choices.sex === sex) { return }
   var characterSVG = document.querySelector('#svg1 .character-container')
   characterSVG.classList.add('character--hide')
-  hash.add({ sex: sex })
-  hash.add({ emotion: 'neutral' }) // Female and Male templates have different set of emotions at this time.
+  window.hash.add({ sex: sex })
+  window.hash.add({ emotion: 'neutral' }) // Female and Male templates have different set of emotions at this time.
   // ^ Should really check to see if the emotion doesn't exist before forcing a change to neutral.
-  if (currentUser && currentUser.cc && currentUser.cc.personnages && currentUser.personnageActuel) {
-    currentUser.cc.personnages[personnageActuel].sex = sex
+  if (window.currentUser && window.currentUser.cc && window.currentUser.cc.personnages && window.currentUser.personnageActuel) {
+    window.currentUser.cc.personnages[window.personnageActuel].sex = sex
   }
   window.sex = sex
   buildCharacter(resetForms)
@@ -18,10 +27,10 @@ function trans (sex) {
 function buildCharacter (callback) {
   var characterSVG = document.querySelector('#svg1')
   setTimeout(function () {
-    zoomFull()
+    window.zoomFull()
     clearForms()
-    clearCharacter()
-    interpretHash()
+    window.clearCharacter()
+    window.interpretHash()
     setTimeout(function () {
       characterSVG.classList.remove('character--hide')
       callback()
@@ -30,23 +39,23 @@ function buildCharacter (callback) {
 }
 
 function hideForms () {
-  hideSidebarLeft()
-  hideSidebarRight()
+  window.hideSidebarLeft()
+  window.hideSidebarRight()
 }
 
 function clearForms () {
-  clearSidebarLeft()
-  clearSidebarRight()
+  window.clearSidebarLeft()
+  window.clearSidebarRight()
 }
 
 function resetForms () {
   hideForms()
   // TODO The following function should be a callback or a response to a promise.
-  createForm()
-  showSidebarLeft()
+  window.createForm()
+  window.showSidebarLeft()
 }
 
-function Character (choices) {
+export function Character (choices) {
   this.choices = choices || {
     emotion: 'neutral',
     body: 'default', // Or 'default' or 'veiny'.
@@ -82,8 +91,8 @@ function Character (choices) {
 
 function modCharacter (myKey, myValue) {
   // look in c.choices to see if the key is already there
-  if (myKey in c.choices) {
-    delete c.choices[myKey]
+  if (myKey in window.c.choices) {
+    delete window.c.choices[myKey]
   };
   if (myKey === 'brows' || myKey === 'eyes' || myKey === 'lashes' || myKey === 'sockets') {
     return
@@ -91,15 +100,16 @@ function modCharacter (myKey, myValue) {
   // If there, modify the value
   // if not, add it in, with the value
   // if the value is '', then delete the key from the object,
-  if (myValue != '') {
-    c.choices[myKey] = myValue
+  if (myValue !== '') {
+    window.c.choices[myKey] = myValue
   };
-  if (currentUser && currentUser.cc && currentUser.cc.personnages && currentUser.cc.personnageActuel) {
-    currentUser.cc.personnages[currentUser.cc.personnageActuel][myKey] = myValue
+  if (window.currentUser && window.currentUser.cc && window.currentUser.cc.personnages && window.currentUser.cc.personnageActuel) {
+    window.currentUser.cc.personnages[window.currentUser.cc.personnageActuel][myKey] = myValue
   }
 };
 
-function createCharacter () {
+/*
+export function createCharacter () {
   document.getElementById(sex + 'Button').checked = true
   // Draw the essential stuff
   // Draw stuff from the hash
@@ -129,6 +139,7 @@ function createCharacter () {
     }
   };
 };
+*/
 
 function GetEmotionGetLayers (option) {
   var faceElements = ['brows', 'eyes', 'mouth', 'lashes', 'sockets']
@@ -137,70 +148,75 @@ function GetEmotionGetLayers (option) {
 
 function getOptions (section) {
   var forms = window.forms
-  var section = capitalizeFirstLetter(section)
-  for (i in forms) {
+  var options
+  section = window.capitalizeFirstLetter(section)
+  for (var i in forms) {
     options = forms[i][section]
-    if (options != undefined) {
+    if (options !== undefined) {
       return options
     }
   }
 }
 
-function show (userChoice, category) {
-  var multiLayer = getMultiLayer()
+export function show (userChoice, category) {
+  var multiLayer = window.getMultiLayer()
+  var sections
   if (typeof (category) === 'string') {
-    var sections = [category]
+    sections = [category]
   } else {
-    var sections = [category.split(' ')[1]]
+    sections = [category.split(' ')[1]]
   };
   var selectedOption = userChoice
   var options = getOptions(sections[0])
-  var obj = new Array()
+  var obj = []
   var id = '#' + sections[0] + '_' + selectedOption
   obj[category] = userChoice
   if (userChoice === '') {
     modCharacter(category, userChoice)
-    hash.remove(category)
+    window.hash.remove(category)
   } else {
     modCharacter(category, userChoice)
-    hash.add(obj)
+    window.hash.add(obj)
   }
-  if (currentUser) {
-    triggerSaveBtn()
+  if (window.currentUser) {
+    window.triggerSaveBtn()
   }
   if (sections[0] === 'emotion') {
     modCharacter(sections[0], selectedOption)
-    ga('send', 'event', 'menu', 'select', id)
+    window.ga('send', 'event', 'menu', 'select', id)
     sections = [] // Reset the sections layer so it doesn't contain 'emotion', as it isn't a layer in itself.
     var emotions = GetEmotionGetLayers(selectedOption)
-    for (emo in emotions) {
-      var newEmo = emotions[emo]
-      sections.push(newEmo)
+    for (var emo in emotions) {
+      sections.push(emotions[emo])
     }
   };
   displaySections(sections, options, selectedOption, multiLayer)
 }
 
 function displaySections (sections, options, selectedOption, multiLayer) {
-  for (section in sections) {
+  for (var section in sections) {
     options.forEach(function (d, i) {
       var id = '#' + sections[section] + '_' + d
-      if (selectedOption != '' && d === selectedOption) {
+      if (selectedOption !== '' && d === selectedOption) {
         sectionShow(multiLayer, id)
 
         if (sections[section] === 'brows' || sections[section] === 'eyes' || sections[section] === 'mouth' || sections[section] === 'lashes' || sections[section] === 'sockets') {
           modCharacter(sections[section], selectedOption)
         } else {
-          var obj = new Array()
+          var obj = []
           obj[sections[section]] = selectedOption
-          hash.add(obj)
+          window.hash.add(obj)
           modCharacter(sections[section], selectedOption)
-          ga('send', 'event', 'menu', 'select', id)
+          window.ga('send', 'event', 'menu', 'select', id)
         }
       } else {
-        for (lyr in multiLayer) {
+        // FIXME: lyr isn't used
+        sectionHide(multiLayer, id)
+        /*
+        for (var lyr in multiLayer) {
           sectionHide(multiLayer, id)
-        };
+        }
+        */
       };
     })
   };
@@ -213,7 +229,7 @@ function sectionShow (multiLayer, id) {
 
   if (id === '#iris_default') { return };
 
-  for (lyr in multiLayer) {
+  for (var lyr in multiLayer) {
     if (id.slice(1) === multiLayer[lyr][0]) {
       isMultiLayered = true
       break
@@ -223,11 +239,12 @@ function sectionShow (multiLayer, id) {
   if (id.slice(1, 7) === 'pupils') {
     pupilShape = id.slice(1).split('_')[1]
     showPupils(pupilShape)
-  } else if (id.slice(1, 5) === 'body' && id.slice(6, 10) != 'head') {
+  } else if (id.slice(1, 5) === 'body' && id.slice(6, 10) !== 'head') {
     var idList = id.split('_')
     var bodySuffix = idList[idList.length - 1]
     var bodyLayers = getBodyLayers(bodySuffix)
     var bodyLayersCounter = bodyLayers.length
+    var idOf
 
     while (bodyLayersCounter--) {
       idOf = '#' + bodyLayers[bodyLayersCounter]
@@ -236,6 +253,7 @@ function sectionShow (multiLayer, id) {
     }
   } else {
     if (isMultiLayered) {
+      var sectionToHide
       for (var i = 1; i <= multiLayer[lyr][1]; i++) {
         idOf = id + '_' + i + '_of_' + multiLayer[lyr][1]
         sectionToHide = svgContainer.querySelector(idOf)
@@ -251,7 +269,7 @@ function sectionShow (multiLayer, id) {
   }
 
   if (id.slice(1).split('_')[0] === 'eyes') {
-    changeClipPathOnEyes(id)
+    window.changeClipPathOnEyes(id)
   }
 }
 
@@ -269,8 +287,8 @@ function getBodyLayers (bodySuffix) {
   return bodyLayers
 }
 
-function getAllBodyLayers () {
-  var layers = getSectionLayersList('body')
+export function getAllBodyLayers () {
+  var layers = window.getSectionLayersList('body')
   var counter = layers.length
   var bodyParts
   var files = []
@@ -301,21 +319,23 @@ function showPupils (pupilShape) {
 function sectionHide (multiLayer, id) {
   var svgContainer = document.querySelector('#svg1')
   var sectionToHide
-  if (id.slice(1) == multiLayer[lyr][0]) {
+
+  // FIXME: where does lyr come from???
+  if (id.slice(1) === multiLayer[lyr][0]) {
     for (var i = 1; i <= multiLayer[lyr][1]; i++) {
-      idOf = id + '_' + i + '_of_' + multiLayer[lyr][1]
-      sectionToHide = svgContainer.querySelector(idOf)
+      sectionToHide = svgContainer.querySelector(id + '_' + i + '_of_' + multiLayer[lyr][1])
       if (sectionToHide != null) {
         sectionToHide.style.opacity = 0
         sectionToHide.style.pointerEvents = 'none'
       }
     }
-  } else if (id.slice(1, 5) === 'body' && id.slice(6, 10) != 'head') {
+  } else if (id.slice(1, 5) === 'body' && id.slice(6, 10) !== 'head') {
     var idList = id.split('_')
     var bodySuffix = idList[idList.length - 1]
     var bodyLayers = getBodyLayers(bodySuffix)
     var bodyLayersCounter = bodyLayers.length
 
+    var idOf
     while (bodyLayersCounter--) {
       idOf = '#' + bodyLayers[bodyLayersCounter]
       svgContainer.querySelector(idOf).style.opacity = 0
